4a02490a7c6814bb98ea87274666ee9c87aa6332
toni haryanto <toha.samba@gmail.com>
2011-11-20 05:24:53 +0700

    set default folder for storing cache

--- application/config/cache.php
+++ application/config/cache.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['cache_dir'] = APPPATH.'cache/default/';
+$config['cache_dir'] = APPPATH.'cache/licache/';
 
 $config['cache_default_expires'] = 0;
 
