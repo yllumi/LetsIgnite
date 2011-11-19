0b973b342dada13602844585f64495ead5472d3a
toni haryanto <toha.samba@gmail.com>
2011-11-19 08:36:28 +0700

    add Modular Extension

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,22 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+require APPPATH."libraries/MX/Controller.php";
+
+class MY_Controller extends MX_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->library('ion_auth');
+		$this->load->library('form_validation');
+		$this->load->library('template');
+		$this->load->library('my_cache');
+		$this->load->library('asset');
+		
+		$this->load->helper('asset');
+
+		$this->user_id = $this->session->userdata('user_id');
+		//$this->data['user'] = $this->ion_auth->profile($user_id);
+	}
+	
+}
