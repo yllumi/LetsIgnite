a12a4c1e1339b0f92ec89f4f4d14495ce25c16c3
toni haryanto <toha.samba@gmail.com>
2011-12-16 08:20:40 +0700

    rename letsignite folder to application for easy understanding

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'system/letsignite';
+	$application_folder = 'system/application';
 
 /*
  *---------------------------------------------------------------
