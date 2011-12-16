1ec8181036cdb9bd65f4c24b2994272fb936e406
toni haryanto <toha.samba@gmail.com>
2011-12-16 15:22:45 +0700

    update ion auth library

--- system/application/modules/auth/models/ion_auth_model.php
+++ system/application/modules/auth/models/ion_auth_model.php
@@ -143,7 +143,7 @@ class Ion_auth_model extends CI_Model
 		$this->load->helper('cookie');
 		$this->load->helper('date');
 		$this->load->library('session');
-		$this->lang->load('ion_auth');
+		$this->lang->load('auth/ion_auth');
 
 		//initialize db tables data
 		$this->tables  = $this->config->item('tables', 'ion_auth');
