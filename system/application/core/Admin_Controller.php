5057e973cb3b50a7b0563fc328a26c3e3d6e819d
toni haryanto <toha.samba@gmail.com>
2011-12-29 11:44:06 +0700

    extends class to MY_Controller, and set template to admin template

--- system/application/core/Admin_Controller.php
+++ system/application/core/Admin_Controller.php
@@ -1,16 +1,33 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-class Admin_Controller extends Authenticated_Controller
-{
-    function __construct()
-    {
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Admin_Controller extends MY_Controller {
+
+    function __construct() {
         parent::__construct();
         
-        /*
-        if($this->data['user']['group'] !== 'admin')
-        {
-            show_error('Shove off, this is for admins.');
+        //if user isn't an admin
+        if (!$this->ion_auth->is_admin()) {
+            show_404();
         }
-        * */
+
+        //set theme to admin
+        $this->template->set_theme('admin');
+        $this->asset->set_theme($this->template->get_theme_path());
+
+        $this->template
+                ->set_partial('header', 'partials/header')
+                ->set_partial('subheader', 'partials/subheader')
+                ->set_partial('metadata', 'partials/metadata')
+                ->set_partial('sidebar', 'partials/sidebar')
+                ->set_partial('footer', 'partials/footer')
+                ->append_metadata(theme_css('layout.css'))
+                ->append_metadata(theme_js('jquery-1.5.2.min.js'))
+                ->append_metadata(theme_js('hideshow.js'))
+                ->append_metadata(theme_js('jquery.tablesorter.min.js'))
+                ->append_metadata(theme_js('jquery.equalHeight.js'));
     }
+
 }
