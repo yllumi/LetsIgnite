6d42984f029a9188261cf2dfa9633a3a99c9658f
toni haryanto <toha.samba@gmail.com>
2011-12-29 18:12:06 +0700

    change dashboard controller to admin controller to show its admin controller, and that is the rule for other module

--- system/application/modules/dashboard/controllers/admin.php
+++ system/application/modules/dashboard/controllers/admin.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Dashboard extends Admin_Controller {
+class Admin extends Admin_Controller {
 
     function __construct() {
         parent::__construct();
