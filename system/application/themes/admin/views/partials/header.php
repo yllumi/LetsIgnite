41bdf8a976f876fe89d713cff254e3ec1aa16ad2
toni haryanto <toha.samba@gmail.com>
2011-12-29 13:07:28 +0700

    add admin theme

---
+++ system/application/themes/admin/views/partials/header.php
@@ -0,0 +1,4 @@
+<hgroup>
+    <h1 class="site_title"><a href="index.html"><?php echo $this->config->item('site_title', 'ion_auth'); ?></a></h1>
+    <h2 class="section_title">Dashboard</h2><div class="btn_view_site"><a href="http://www.medialoot.com">View Site</a></div>
+</hgroup>
---
+++ system/application/themes/admin/views/partials/subheader.php
@@ -0,0 +1,7 @@
+<div class="user">
+    <p><?php echo $this->session->userdata('username'); ?></p>
+    <!-- <a class="logout_user" href="#" title="Logout">Logout</a> -->
+</div>
+<div class="breadcrumbs_container">
+    <article class="breadcrumbs"><a href="index.html">Website Admin</a> <div class="breadcrumb_divider"></div> <a class="current">Dashboard</a></article>
+</div>
