a1730eba9a1095887ce24092bee03b367c36dd63
toni haryanto <toha.samba@gmail.com>
2011-12-29 20:14:54 +0700

    add breadcrumb captions

--- system/application/themes/admin/views/partials/subheader.php
+++ system/application/themes/admin/views/partials/subheader.php
@@ -3,5 +3,17 @@
     <!-- <a class="logout_user" href="#" title="Logout">Logout</a> -->
 </div>
 <div class="breadcrumbs_container">
-    <article class="breadcrumbs"><a href="index.html">Website Admin</a> <div class="breadcrumb_divider"></div> <a class="current">Dashboard</a></article>
+    <article class="breadcrumbs">
+		<a href="index.html"><?php echo $this->config->item('site_title', 'ion_auth'); ?></a> 
+		<div class="breadcrumb_divider"></div> <a class="current"><?php echo $template['title']; ?></a>
+		<?php if(isset($template['breadcrumbs'][0]['name'])): ?>
+			<div class="breadcrumb_divider"></div> <a class="current"><?php echo $template['breadcrumbs'][0]['name']; ?></a>
+		<?php endif; ?>
+		<?php if(isset($template['breadcrumbs'][1]['name'])): ?>
+			<div class="breadcrumb_divider"></div> <a class="current"><?php echo $template['breadcrumbs'][1]['name']; ?></a>
+		<?php endif; ?>
+		<?php if(isset($template['breadcrumbs'][2]['name'])): ?>
+			<div class="breadcrumb_divider"></div> <a class="current"><?php echo $template['breadcrumbs'][2]['name']; ?></a>
+		<?php endif; ?>
+    </article>
 </div>
