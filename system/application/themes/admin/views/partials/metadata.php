42a8af1138b0e2252f89fe904afa9c56a9da3fee
toni haryanto <toha.samba@gmail.com>
2012-04-18 12:36:20 +0700

    fix url for rel

--- system/application/themes/admin/views/partials/metadata.php
+++ system/application/themes/admin/views/partials/metadata.php
@@ -14,8 +14,8 @@
     <![endif]-->
 
     <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="../assets/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="<?php echo theme_image_path('ico/favicon.ico'); ?>">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo theme_image_path('ico/apple-touch-icon-114-precomposed.png'); ?>">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo theme_image_path('ico/apple-touch-icon-72-precomposed.png'); ?>">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo theme_image_path('ico/apple-touch-icon-57-precomposed.png'); ?>">
 
